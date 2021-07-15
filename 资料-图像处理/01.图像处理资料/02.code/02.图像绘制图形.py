465e3cbd52dd810a6b9508b876ced80e0bdbfe25
GuangwenSi <45747353+GuangwenSi@users.noreply.github.com>
2021-07-15 09:34:03 +0800

    添加02ffmpeg文件----主要是视频转码格式转换的一些常用操作

--- 资料-图像处理/01.图像处理资料/02.code/02.图像绘制图形.py
+++ 资料-图像处理/01.图像处理资料/02.code/02.图像绘制图形.py
@@ -7,11 +7,12 @@ img = np.zeros((512,512,3),np.uint8)
 
 # 2 绘制图形
 cv.line(img,(0,0),(511,511),(255,0,0),5)
-cv.circle(img,(256,256),60,(0,0,255),-1)
-cv.rectangle(img,(100,100),(400,400),(0,255,0),5)
+cv.circle(img,(200,200),70,(0,0,255),-1)
+cv.rectangle(img,(100,200),(300,400),(0,0,255),10)  # 定位顶点的位置
 cv.putText(img,"hello",(100,150),cv.FONT_HERSHEY_COMPLEX,5,(255,255,255),3)
 
 
 # 3 显示结果
-plt.imshow(img[:,:,::-1])
+# plt.imshow(img[:,:,::-1])
+plt.imshow(img[:,:,::-1])  # 所有元素反转  rgb  bgr
 plt.show()
